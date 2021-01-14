8eea5e9478b221279959493644087aa41fd52a79
Chris Barker <Chris.Barker@noaa.gov>
2021-01-13 17:29:30 -0800

    increment version number

--- lat_lon_parser/__init__.py
+++ lat_lon_parser/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 
 __author__ = """Christopher Barker"""
 __email__ = 'Chris.Barker@noaa.gov'
-__version__ = '1.1.4'
+__version__ = '1.2.0'
 
 from .parser import parse
 from .lat_long import (to_dec_deg, to_deg_min, to_deg_min_sec, to_str_dec_deg,
